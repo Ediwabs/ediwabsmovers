6504fe50c8b22365896bea55fcf2b0c32f4d0422
unknown <dejaneediwabs@gmail.com>
2024-08-15 08:21:13 -0500

    first web update

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Back to Top Button
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+// Show the button when scrolled down 100px
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// Scroll to top when the button is clicked
+backToTopBtn.addEventListener('click', function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
